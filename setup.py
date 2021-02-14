cc1c0240094bf1382ec1701d7d6e412ea4b9b1fd
harrytwigg <harrytwigg111@gmail.com>
2021-02-14 16:17:28 +0000

    0.0.3

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from distutils.core import setup
 setup(
     name='viaduct',         # How you named your package folder (MyLib)
     packages=['viaduct'],   # Chose the same as "name"
-    version='0.0.2',      # Start with a small number and increase it with every change you make
+    version='0.0.3',      # Start with a small number and increase it with every change you make
     # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-    license='	gpl-3.0',
+    license='gpl-3.0',
     # Give a short description about your library
     description='Viaduct is a Trading212 REST API wrapper for python',
     long_description="Viaduct is a Python REST API wrapper that utilises Trading212's REST API that normally communciates exclusively with first party apps. This has not been offically released publicly but can be reverse engineered. Selenium is used to scrape required cookies and customer data from the web app but is no longer required after this point. REST network calls are used enabling greater functionallity and speed than pure web scraping. The use of a wrapper ensures any API changes in the future will not impact pre-existing Trading212 dependeant programs as the wrapper itself can be updated instead of the underlying code.",
@@ -14,7 +14,7 @@ setup(
     # Provide either the link to your github or to your website
     url='https://github.com/harrytwigg/Viaduct-Trading212-Python-API',
     # I explain this later on
-    download_url='https://github.com/harrytwigg/Viaduct-Trading212-Python-API/archive/0.0.2.tar.gz',
+    download_url='https://github.com/harrytwigg/Viaduct-Trading212-Python-API/archive/0.0.3.tar.gz',
     keywords=['python', 'api', 'rest', 'api-wrapper', 'viaduct', 'trading212',
               'trading212-api'],   # Keywords that define your package best
     install_requires=[            # I get to this in a second
